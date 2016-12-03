3eac8b477b4be6bb58555752d1f9a2c173e4c3f8
kgimhg <kgimhg@naver.com>
2016-12-03 20:13:07 +0900

    Add files via upload

--- a.c
+++ a.c
@@ -5,33 +5,36 @@
 #include<time.h>
 
 char bootblock[2];  //  부트블럭은 16비트 고정
-/*typedef struct
+typedef struct
 {
 	int direct;		//직접
 	struct data *indirect;  //간접 자기참조구조체
 }data;
-*/
+
 typedef struct 
 {
 	int c_i_bi[16];		// i-node 사용여부 (512)
 	int c_d_bit[32];	// data block 사용여부 (1024)
 }supers;
-typedef struct 
+
+typedef struct
 {
-	_Bool file; 					// 0-file, 1-dir
+	_Bool file; 					// 0-file, 1-dir 
 	time_t now;						// 생성시간
 	int size;     					// 파일크기
 	//data num;						// 데이터 블럭 번호
 /*
-	int *direct;
+	struct i_node *direct;
 	int *s_indirect;
 	int *d-indirect;
  */
 }i_node[512];
+
 typedef struct
 {
 	char data[128];				// 128바이트
 }data_block[1024];
+
 typedef struct
 {
 	char *l_link;	// 왼쪽 파일 링크
@@ -44,6 +47,8 @@ typedef struct
 
    슈퍼블럭 inode 비트열 찾기
    슈퍼블럭 data 비트열 찾기
+
+
 
 
 i_node get_f_inode(char z)     //파일의 inode생성
@@ -86,40 +91,84 @@ int main()
 				system("touch myfs");	//파일 만들기
 		}
 		if(strcmp("myfs_shell", com[0])==0)
-			break;
-	}
-	if((ifp=fopen("myfs", "r"))!=NULL)		//myfs 파일 유무 검사
-	{
-		if(strcmp("myfs_shell", com[0])==0)
 		{
-			while(1)
+			if((ifp=fopen("myfs", "r"))==NULL)		//myfs 파일 유무 검사
+				printf("error : myfs not exists.\n");
+		}
+		if((ifp=fopen("myfs", "r"))!=NULL)		//myfs 파일 유무 검사
+		{
+		/*ifp=fopen("myfs", "rb");			//이진파일(저장내용) 불러오기
+		
+		 {
+		   	* 파일 불러오는 과정 *
+		 }
+
+		 */
+
+			if(strcmp("myfs_shell", com[0])==0)
 			{
-				printf("[ %s]$ ", dir);	//현재 위치 출력
-				for(i=0;i<4;i++)
+				while(1)
 				{
-					scanf("%s", com[i]);       //명령어 입력
-					c=getchar();
-					if(c=='\n')
+					printf("[%s]$ ", dir);	//현재 위치 출력
+					for(i=0;i<4;i++)
+					{
+						scanf("%s", com[i]);       //명령어 입력
+						c=getchar();
+						if(c=='\n')
+							break;
+					}
+					if(strcmp("myls", com[0])==0)
+					{
+						//이진트리
+					}
+
+					if(strcmp("mycpto", com[0])==0)
+					{
+						FILE *afp;
+						char c[30];
+						sprintf(c, "touch %s", com[2]);			//프롬프트에 파일생성 명령어 만들기
+						system(c);								//파일 생성
+						//내용 쓰기?
+					}
+					if(strcmp("mycd", com[0])==0)
+					{
+					/*if(strcmp(com[1],"NULL")==0)
+							strcpy(dir, "/  ");
+						else
+					*/		strcpy(dir, com[1]); 
+					}
+					if(strcmp("mypwd", com[0])==0)
+						;
+	
+					if(strcmp("mytouch", com[0])==0)
+					{
+						
+					}
+					if(strcmp("mymkdir", com[0])==0)
+					{
+
+					}
+					if(strcmp("mystate", com[0])==0)
+					{
+					//	printf("free inode : %d", );		//inode 남은갯수
+					//	printf("free data block : %d", );	//data block 남은갯수
+					}
+					/* command */
+					if(strcmp("ls", com[0])==0)
+					{
+						if(strcmp("-l", com[1])==0)
+							system("ls -l");
+						else if(strcmp("-i", com[1])==0)
+							system("ls -i");
+						else if(strcmp("-ii", com[1])==0)
+							system("ls -il");
+						else
+							system("ls");
+					}
+					if(strcmp("byebye", com[0])==0)
 						break;
-				}
-				if(strcmp("ls", com[0])==0)
-				{
-					if(strcmp("-l", com[1])==0)
-						system("ls -l");
-					else if(strcmp("-i", com[1])==0)
-						system("ls -i");
-					else if(strcmp("-ii", com[1])==0)
-						system("ls -il");
-					else
-						system("ls");
-				}
-				if(strcmp("mycd", com[0])==0)
-					strcpy(dir, com[1]); 
-				if(strcmp("mypwd", com[0])==0)
-					printf("%s\n", dir);
-				if(strcmp("byebye", com[0])==0)
-					break;
-			}			
-		}	
+				}			
+			}		
+		}
 	}
 }
